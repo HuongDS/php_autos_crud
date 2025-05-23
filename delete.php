41870e28197654c2d8bd78a49934946831417a2f
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-23 22:02:25 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -38,7 +38,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Ambika Patidar's Autos Database fb00771a a04e8bd0</title>
+    <title>Ambika Patidar's Autos Database fb00771a a04e8bd0 b3d29210</title>
 
     <?php require_once "bootstrap.php"; ?>
 
