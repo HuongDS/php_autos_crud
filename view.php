b1ace02729eede53ca8fcd55286fc605c0e0b16a
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-23 22:04:31 +0700

    Update view.php

--- view.php
+++ view.php
@@ -13,7 +13,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Ambika Patidar's Login Page</title>
+<title>Ambika Patidar's Login Page b3d29210</title>
 
 <?php require_once "bootstrap.php"; ?>
 
